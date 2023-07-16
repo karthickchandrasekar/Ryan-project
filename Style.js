b05d602f9eb2f2ef986a33b72a644504ef27061c
karthickchandrasekar <karthickchandrasekaran2000@gmail.com>
2023-07-16 13:48:04 -0700

    implement javascript

--- Style.js
+++ Style.js
@@ -12,3 +12,25 @@ btn.forEach(bt=>{
     }
 });
 });
+
+
+const sliderbtn=document.querySelectorAll(".slider-arrow i");
+const cardwidth=document.querySelector(".card").offsetWidth;
+const cardslider=document.querySelectorAll(".card-slider");
+sliderbtn.forEach(arrow=>{
+    arrow.addEventListener("click",()=>{
+        //console.log(arrow);
+        //console.log(arrow.getAttribute("data-action"));
+        cardslider.forEach(slid=>{
+            if(slid.getAttribute("data-target")==arrow.getAttribute("data-slider")){
+                if(arrow.getAttribute("data-action")=="left"){
+                    slid.scrollLeft+=cardwidth;
+                }
+                if(arrow.getAttribute("data-action")=="right"){
+                    slid.scrollLeft-=cardwidth;
+                }
+            }
+        })
+
+    })
+})
