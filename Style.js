087c2ed711c7902ba781e9705cc045bfa88664cd
karthickchandrasekar <karthickchandrasekaran2000@gmail.com>
2023-07-16 08:33:01 -0700

    main change

--- Style.js
+++ Style.js
@@ -5,10 +5,10 @@ console.log(img.offsetWidth);
 btn.forEach(bt=>{
     bt.addEventListener("click",()=>{
     if(bt.id=="left"){
-    slider.scrollLeft+=img.offsetWidth;
+    slider.scrollLeft-=img.offsetWidth;
     }
     else{
-        slider.scrollLeft-=img.offsetWidth;
+        slider.scrollLeft+=img.offsetWidth;
     }
 });
 });
@@ -24,13 +24,55 @@ sliderbtn.forEach(arrow=>{
         cardslider.forEach(slid=>{
             if(slid.getAttribute("data-target")==arrow.getAttribute("data-slider")){
                 if(arrow.getAttribute("data-action")=="left"){
-                    slid.scrollLeft+=cardwidth;
+                    slid.scrollLeft-=cardwidth;
                 }
                 if(arrow.getAttribute("data-action")=="right"){
-                    slid.scrollLeft-=cardwidth;
+                    slid.scrollLeft+=cardwidth;
                 }
             }
         })
 
     })
 })
+const form=document.querySelector("form");
+
+form.addEventListener("submit",(e)=>{
+
+  e.preventDefault();
+  const email=document.getElementById("email").value;
+  const name=document.getElementById("name").value;
+  const idea=document.getElementById("idea").value;
+  const body= "name :"+name+"<br>"+"email :"+email+"<br>"+"idea :"+idea;
+  Email.send({
+    //SecureToken:"e98a6df6-3657-4c1d-8006-926c65caeb44",
+   Host : "smtp.elasticemail.com",
+  Username : "t85088657@gmail.com",
+  Password : "199C7A3FCC0726871349BCFD4298936AF122",
+    To : email,
+    From : "t85088657@gmail.com",
+    Subject : "mail from portfolio",
+    Body : "Thanks for your Mail,response will sent with in 48 hours. <br>" + body
+}).then(
+  message => alert(message)
+);
+
+})
+
+const bars=document.getElementById("bars");
+const close= document.getElementById("close");
+const menu=document.getElementById("menu-item");
+
+bars.addEventListener("click",()=>{
+   menu.style.display="block";
+   close.style.display="block";
+   bars.style.display="none";
+   menu.style.width="auto";
+   menu.style.height="120vh";
+});
+
+close.addEventListener("click",()=>{
+
+    menu.style.display="none";
+   close.style.display="none";
+   bars.style.display="block";
+});
